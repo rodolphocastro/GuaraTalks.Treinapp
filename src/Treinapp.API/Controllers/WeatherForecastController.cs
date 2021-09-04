6e7223862d75525d9b0a6af19a6cbc1a9bdc3ad1
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-09-04 11:00:13 -0300

    refactor: Consolidate routes into consts

--- src/Treinapp.API/Controllers/WeatherForecastController.cs
+++ src/Treinapp.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Treinapp.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route(Constants.DefaultRoute)]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- src/Treinapp.API/Startup.cs
+++ src/Treinapp.API/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Treinapp.API
 {
@@ -55,5 +48,16 @@ namespace Treinapp.API
                 endpoints.MapControllers();
             });
         }
+    }
+
+    /// <summary>
+    /// Constants applicable to the API as a whole.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Default route for controllers.
+        /// </summary>
+        public const string DefaultRoute = @"[controller]";
     }
 }
