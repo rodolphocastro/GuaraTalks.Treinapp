53a4a2033b9653bad86a03f6ac2f0023309476ab
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-09-04 19:55:44 -0300

    feat: Scaffold API shell

---
+++ src/Treinapp.API/Features/Sports/SportsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Treinapp.API.Features.Sports
+{
+    [ApiController]
+    [Route(Constants.DefaultRoute)]
+    public class SportsController : ControllerBase
+    {
+        private CancellationToken _token => HttpContext?.RequestAborted ?? default;
+
+        [HttpGet]
+        public async Task<IActionResult> ListAll()
+        {
+            // TODO: Implement the Endpoint
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNew()
+        {
+            // TODO: Implement the Endpoint
+            return CreatedAtAction(nameof(ListAll), null);
+        }
+    }
+}
---
+++ src/Treinapp.API/Features/Workouts/WorkoutsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace Treinapp.API.Features.Workouts
+{
+    [ApiController]
+    [Route(Constants.DefaultRoute)]
+    public class WorkoutsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> ListAll()
+        {
+            // TODO: Implement the Endpoint
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BookNew()
+        {
+            // TODO: Implement the Endpoint
+            return CreatedAtAction(nameof(ListAll), null);
+        }
+
+        [HttpPut("begin")]
+        public async Task<IActionResult> Begin()
+        {
+            // TODO: Implement the Endpoint
+            return AcceptedAtAction(nameof(ListAll), null);
+
+        }
+
+        [HttpPut("finish")]
+        public async Task<IActionResult> Finish()
+        {
+            // TODO: Implement the Endpoint
+            return AcceptedAtAction(nameof(ListAll), null);
+        }
+    }
+}
