6cf2a6031ffcf8444321d74b965f1b08806b09d5
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-09-05 22:47:30 -0300

    feat: Describe a Workout

---
+++ src/Treinapp.API/Features/Workouts/Workout.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Treinapp.API.Features.Workouts
+{
+    /// <summary>
+    /// A Workout controlled by this API,
+    /// "Creating" a Workout is "Booking" it as well.
+    /// A workout exists within a Sport, never on its own.
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <param name="BookedAt"></param>
+    /// <param name="StartedAt"></param>
+    /// <param name="FinishedAt"></param>
+    public record Workout(Guid Id, DateTimeOffset BookedAt, DateTimeOffset? StartedAt, DateTimeOffset? FinishedAt)
+    {
+        /// <summary>
+        /// Whatever a Workout has been started.
+        /// </summary>
+        public bool IsStarted => StartedAt.HasValue;
+
+        /// <summary>
+        /// Whatever a Workout has been finished.
+        /// </summary>
+        public bool IsFinished => FinishedAt.HasValue;
+
+        /// <summary>
+        /// Starts a Workout session.
+        /// </summary>
+        /// <returns></returns>
+        public Workout Start() => this with { StartedAt = DateTimeOffset.UtcNow };
+
+        /// <summary>
+        /// Finishes a Workout session.
+        /// </summary>
+        /// <returns></returns>
+        public Workout Finish() => this with { FinishedAt = DateTimeOffset.UtcNow };
+    }
+}
