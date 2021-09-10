302fc2e8793c7a44db322375492e50ceda261bdd
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-09-10 17:26:00 -0300

    refactor: Move Report to the Domain namespace

--- src/Treinapp.Reports.Worker/Features/Reports/Report.cs
+++ src/Treinapp.Reports.Worker/Features/Reports/Report.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 using Treinapp.Commons.Domain;
 
-namespace Treinapp.Reports.Worker.Features.Reports
+namespace Treinapp.Common.Domain
 {
     public record Report
     {
--- src/Treinapp.Reports.Worker/Features/Reports/ReportPersistence.cs
+++ src/Treinapp.Reports.Worker/Features/Reports/ReportPersistence.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Treinapp.Common.Domain;
 using Treinapp.Commons.Domain;
 
 namespace Treinapp.Reports.Worker.Features.Reports
