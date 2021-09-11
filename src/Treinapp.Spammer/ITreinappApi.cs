d984aabc7c08a7e28169c92ec184908b712a5b74
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-09-11 00:34:59 -0300

    feat: Describe a routine to create a Sport

---
+++ src/Treinapp.Spammer/ITreinappApi.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using Treinapp.Commons.Domain;
+
+namespace Treinapp.Spammer
+{
+    public class CreateSportPayload
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public interface ITreinappApi
+    {
+        [Post("/sports")]
+        Task<Sport> CreateNew([Body] CreateSportPayload command, CancellationToken cancellationToken = default);
+    }
+}
