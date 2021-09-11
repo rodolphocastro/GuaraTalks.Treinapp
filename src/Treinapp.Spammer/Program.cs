c564e9c5411e5fdc2550afe28457aa98747e6234
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-09-10 23:38:47 -0300

    feat: App shell for Spammer

---
+++ src/Treinapp.Spammer/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Treinapp.Spammer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var configuration = hostContext.Configuration;
+                    // TODO: Setup MediatR
+                    // TODO: Setup Refit
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
